8af1dda373c459cbbd2baa5bc50020e2db8a75f0
Nerwin Hernando <nerwinc.hernando@gmail.com>
2018-10-30 15:57:07 +0800

    wire action creator

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { emailChanged } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends React.Component {
+    onEmailChange(text) {
+        this.props.emailChanged(text);
+    }
     render() {
         return(
             <Card>
@@ -9,6 +14,7 @@ class LoginForm extends React.Component {
                     <Input
                         label="Email"
                         placeholder="email@gmail.com"
+                        onChangeText={this.onEmailChange.bind(this)}
                     />
                 </CardSection>
                 <CardSection>
@@ -28,4 +34,4 @@ class LoginForm extends React.Component {
     }
 };
 
-export default LoginForm;
+export default connect(null, { emailChanged })(LoginForm);
