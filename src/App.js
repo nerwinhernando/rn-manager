28197ab995b8b8cd3cd15bc0a3486fe5dc38121c
Nerwin Hernando <nerwinc.hernando@gmail.com>
2018-10-30 15:21:25 +0800

    boilerplate setup

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends React.Component {
+    componentWillMount() {
+        var config = {
+            apiKey: 'AIzaSyBbLXCUZJUhH2Nw6_knEoekGQF0YDLMuZ0',
+            authDomain: 'manager-ab850.firebaseapp.com',
+            databaseURL: 'https://manager-ab850.firebaseio.com',
+            projectId: 'manager-ab850',
+            storageBucket: 'manager-ab850.appspot.com',
+            messagingSenderId: '145422229395'
+        };
+        firebase.initializeApp(config);
+    }
+
     render() {
         return(
             <Provider store={createStore(reducers)}>
