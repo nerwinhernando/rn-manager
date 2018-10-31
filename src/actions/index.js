315db539d263b7859111eab85600857e4b4a9b7b
Nerwin Hernando <nerwinc.hernando@gmail.com>
2018-10-31 14:50:34 +0800

    loginUser action

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,3 +16,8 @@ export const passwordChanged = (text) => {
         payload: text
     }
 };
+
+export const loginUser = ( { email, password }) => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(user => console.log(user));
+};
