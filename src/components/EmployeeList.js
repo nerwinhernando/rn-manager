ee8440e90a2be1c54a7ea95ca9a8dd32236337fe
Nerwin Hernando <nerwinc.hernando@gmail.com>
2018-11-02 17:14:39 +0800

    EmployeeReducer

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { View, Text } from 'react-native';
+import { employeesFetch } from "../actions";
 
 class EmployeeList extends React.Component {
+    componentWillMount() {
+        this.props.employeesFetch();
+    }
+
     render() {
         return(
             <View>
@@ -16,4 +22,4 @@ class EmployeeList extends React.Component {
     }
 }
 
-export default EmployeeList;
+export default connect(null, { employeesFetch })(EmployeeList);
