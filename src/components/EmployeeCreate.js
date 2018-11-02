ec0d1b1138ae8b1bed4bd7c5bbe5e0e2a8658f3c
Nerwin Hernando <nerwinc.hernando@gmail.com>
2018-11-02 16:39:38 +0800

    default form values

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -8,7 +8,7 @@ class EmployeeCreate extends React.Component {
     onButtonPress() {
         const { name, phone, shift } = this.props;
 
-        this.props.employeeCreate({ name, phone, shift });
+        this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
     }
 
     render() {
