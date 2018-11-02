b1b6f3966940b248b619c0e9db38f6007e697966
Nerwin Hernando <nerwinc.hernando@gmail.com>
2018-11-02 15:56:03 +0800

    Picker component

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions'
+import { employeeUpdate } from '../actions';
+import { Picker } from 'react-native';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends React.Component {
@@ -23,6 +24,22 @@ class EmployeeCreate extends React.Component {
                         value={this.props.phone}
                         onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value }) }
                     />
+                </CardSection>
+
+                <CardSection>
+                    <Picker
+                        style={{ flex: 1}}
+                        selectedValue={this.props.shift}
+                        onValueChange={value => this.props.employeeUpdate({ prop: 'shift', value})}
+                    >
+                        <Picker.Item label="Monday" value="Monday" />
+                        <Picker.Item label="Tuesday" value="Tuesday" />
+                        <Picker.Item label="Wednesday" value="Wednesday" />
+                        <Picker.Item label="Thursday" value="Thursday" />
+                        <Picker.Item label="Friday" value="Friday" />
+                        <Picker.Item label="Saturday" value="Saturday" />
+                        <Picker.Item label="Sunday" value="Sunday" />
+                    </Picker>
                 </CardSection>
 
                 <CardSection>
